c8d524d0b1925e75d6230e39195d0d991d20adff
f304646673 <f304646673@example.com>
2017-01-20 01:18:11 +0800

    modify

--- src/strategy/update_stock_daily_ex_dividend_info.py
+++ src/strategy/update_stock_daily_ex_dividend_info.py
@@ -109,8 +109,8 @@ class update_stock_daily_ex_dividend_info(job_base):
     def _get_all_share_ids(self):
         date_info = time.strftime('%Y_%m_%d')
         trade_table_name = "trade_info_%s" % (date_info)
-        share_ids = fetch_data.get_data(fetch_data.select_db("daily_temp", trade_table_name, ["share_id"],{"share_id":["000001","="]}, pre = "distinct"))
-        #share_ids = fetch_data.get_data(fetch_data.select_db("daily_temp", trade_table_name, ["share_id"], {}, pre = "distinct"))
+        #share_ids = fetch_data.get_data(fetch_data.select_db("daily_temp", trade_table_name, ["share_id"],{"share_id":["000001","="]}, pre = "distinct"))
+        share_ids = fetch_data.get_data(fetch_data.select_db("daily_temp", trade_table_name, ["share_id"], {}, pre = "distinct"))
         return share_ids
 
     def _create_table_if_not_exist(self, share_id, table_name):
